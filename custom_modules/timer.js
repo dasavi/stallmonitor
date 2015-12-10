143e798184ef1a6490dbf15e579d46e0f4b5681e
avishek.das <avishekd@slalom.com>
2015-12-10 10:30:04 -0600

    Made stall status expiration time configurable via environment variable

--- custom_modules/timer.js
+++ custom_modules/timer.js
@@ -1,8 +1,15 @@
 /**
 *  This module keeps track of timers for each stall
 */
+var _ = require('lodash');
+
+var DEFAULT_EXPIRE_TIME = 10000
+var STATUS_EXPIRE_TIME = process.env.STATUS_EXPIRE_TIME;
+var RESET_INTERVAL = _.isNull(STATUS_EXPIRE_TIME) || _.isUndefined(STATUS_EXPIRE_TIME) ? 
+DEFAULT_EXPIRE_TIME : STATUS_EXPIRE_TIME;
+
+//Object containing timers for all active stalls
 var timers ={};
-var RESET_INTERVAL = 60000;
 
 var getKey = function(floor, bathroom, stallName){
 	return floor + bathroom + stallName;
