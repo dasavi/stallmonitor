bf99181addd11b3fc2df9a4f7bcdb402f334dcdc
Burton Podczerwinski <burtonp@slalom.com>
2016-01-04 12:20:11 -0600

    - implementing svg and css - misc cleanup

--- stallmonitor/js/modules/main-view.js
+++ stallmonitor/js/modules/main-view.js
@@ -40,5 +40,9 @@ mainView.controller('mainViewCtrl', ['$scope', '$firebaseObject', '$http', 'DB_U
 
             $http.post(notifyEndpoint, data);  
         };
+
+        $scope.stallClicked = function(event, stall) {
+            alert("Stall " + stall + " Clicked!");
+        };
     }
 ]);
