07d3d3a907936bece7fa783e9b76d8a182ef4b92
Avishek Das <suavidas@gmail.com>
2016-09-29 17:51:21 -0500

    Don't show meta fields

--- custom_modules/database.js
+++ custom_modules/database.js
@@ -4,12 +4,18 @@ var databaseUrl = process.env.DB_URL;
 var myFirebaseRef = new Firebase(databaseUrl);
 console.log("Using database: " + databaseUrl);
 
+var bathroomFieldName = "bathrooms";
+var spacesFieldName = "spaces";
+var notifyFieldName = "notify";
+
+exports.fields = {bathroomFieldName, spacesFieldName, notifyFieldName};
+
 exports.getStallReference = function(floor, bathroom, stallName){
-	return myFirebaseRef.child(floor).child(bathroom).child("spaces").child(stallName);	
+	return myFirebaseRef.child(floor).child(bathroomFieldName).child(bathroom).child(spacesFieldName).child(stallName);	
 };
 
 exports.getNotifyReference = function(floor, bathroom){
-	return myFirebaseRef.child(floor).child(bathroom).child("notify");	
+	return myFirebaseRef.child(floor).child(bathroomFieldName).child(bathroom).child(notifyFieldName);	
 };
 
 exports.getAllStalls = function() {
