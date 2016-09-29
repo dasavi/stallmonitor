2295dd168da14999952f982a22b361d5891f730c
Avishek Das <suavidas@gmail.com>
2016-09-29 17:55:20 -0500

    Comment out unimplemented db path

--- custom_modules/database.js
+++ custom_modules/database.js
@@ -4,18 +4,18 @@ var databaseUrl = process.env.DB_URL;
 var myFirebaseRef = new Firebase(databaseUrl);
 console.log("Using database: " + databaseUrl);
 
-var bathroomFieldName = "bathrooms";
+// var bathroomFieldName = "bathrooms";
 var spacesFieldName = "spaces";
 var notifyFieldName = "notify";
 
-exports.fields = {bathroomFieldName, spacesFieldName, notifyFieldName};
+// exports.fields = {bathroomFieldName, spacesFieldName, notifyFieldName};
 
 exports.getStallReference = function(floor, bathroom, stallName){
-	return myFirebaseRef.child(floor).child(bathroomFieldName).child(bathroom).child(spacesFieldName).child(stallName);	
+	return myFirebaseRef.child(floor)/*.child(bathroomFieldName)*/.child(bathroom).child(spacesFieldName).child(stallName);
 };
 
 exports.getNotifyReference = function(floor, bathroom){
-	return myFirebaseRef.child(floor).child(bathroomFieldName).child(bathroom).child(notifyFieldName);	
+	return myFirebaseRef.child(floor)/*.child(bathroomFieldName)*/.child(bathroom).child(notifyFieldName);
 };
 
 exports.getAllStalls = function() {
